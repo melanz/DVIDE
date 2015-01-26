39d63d8ac02daa07a58cba5abadaccbd0aa82176
Daniel Melanz <daniel.melanz@gmail.com>
2015-01-25 22:00:03 -0600

    Changed Element to Body

---
+++ src/Body.cuh
@@ -0,0 +1,109 @@
+/*
+ * Body.cuh
+ *
+ *  Created on: Sep 20, 2013
+ *      Author: melanz
+ */
+
+#ifndef BODY_CUH_
+#define BODY_CUH_
+
+#include "include.cuh"
+
+class Body {
+  friend class System;
+private:
+  uint identifier;
+	uint index;
+	int numDOF;
+
+	double3 pos;
+	double3 vel;
+	double3 acc;
+
+	double mass;
+
+	bool fixed;
+	double3 geometry;
+
+public:
+	Body() {
+	  numDOF = 3;
+	  identifier = 0;
+	  index = 0;
+
+		// create test element!
+		pos = make_double3(0, 0, 0);
+		vel = make_double3(0, 0, 0);
+		acc = make_double3(0, 0, 0);
+
+		mass = 1.0;
+
+		fixed = false;
+		geometry = make_double3(1.0,0,0);
+	}
+
+  Body(double3 position) {
+    numDOF = 3;
+    identifier = 0;
+    index = 0;
+
+    // create test element!
+    pos = position;
+    vel = make_double3(0, 0, 0);
+    acc = make_double3(0, 0, 0);
+
+    mass = 1.0;
+
+    fixed = false;
+    geometry = make_double3(1.0,0,0);
+  }
+
+  bool isFixed()
+  {
+    return fixed;
+  }
+
+	double3 getPosition()
+	{
+	  return pos;
+	}
+
+  double3 getVelocity()
+  {
+    return vel;
+  }
+
+  double getMass()
+  {
+    return mass;
+  }
+
+  uint getIndex()
+  {
+    return index;
+  }
+
+	void setIndex(uint index) {
+		this->index = index;
+	}
+
+	void setIdentifier(uint identifier) {
+		this->identifier = identifier;
+	}
+
+  void setBodyFixed(bool fixed)
+  {
+    this->fixed = fixed;
+  }
+  double3 getGeometry()
+  {
+    return geometry;
+  }
+  void setGeometry(double3 geometry)
+  {
+    this->geometry = geometry;
+  }
+};
+
+#endif /* ELEMENT_CUH_ */
