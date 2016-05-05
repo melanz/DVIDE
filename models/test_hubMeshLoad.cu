bf74bdfa0cd2daad33858432fe819257023513fc
Daniel Melanz <daniel.melanz@gmail.com>
2016-05-05 15:58:41 -0500

    Minor test change

--- models/test_hubMeshLoad.cu
+++ models/test_hubMeshLoad.cu
@@ -336,7 +336,7 @@ int main(int argc, char** argv)
 #ifdef WITH_GLUT
   bool visualize = true;
 #endif
-  //visualize = false;
+  visualize = false;
 
   sys = new System(solverTypeQOCC);
   sys->setTimeStep(hh);
